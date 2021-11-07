fcbd7bc616f6427030d37f09457ae20c0e536529
consta <mail@kanstantin.com>
2021-11-07 17:09:30 +0300

    Added table with emotion cards (based on buttons)

--- elog/ViewController.swift
+++ elog/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cards: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for btn in cards {
+            let index = cards.firstIndex(of: btn)!
+            let title: String = "\(index)"
+            btn.setTitle(title, for: UIControl.State.normal)
+        }
     }
 
 
